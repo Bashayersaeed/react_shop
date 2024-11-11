daa1098a28f475e2713cae9b0f31b83b981c879e
Bashayersaeed <bsaeed83@ymail.com>
2024-11-11 20:28:26 +0300

    I could not submit yesterday du to git push issues

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import './App.css';
 import storeImage from "./storeImage.jfif";
 
-function App() {
-  return (
-    <div className="App">
-<h1>Chocolate Lovers</h1>
-<p>A World Of Happiness</p>
-<img className= "Store-image" alt= "storeImage" src={storeImage}></img>
-    </div>
 
-  );
+function App() {
+  
+return(<div className="App">
+  <h1>Chocolate Lovers</h1>
+  <p>A World Of Happiness</p>
+  <img className= "Store-Image" alt= "storeImage" src={storeImage}></img>  
+</div>)
+ 
 }
 
 export default App;
